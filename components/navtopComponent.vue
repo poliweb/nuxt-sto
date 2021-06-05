afc53d735a17710a12de1aef7a7ab3e8b77a41f7
web@poliweb.su <web@poliweb.su>
2021-06-05 22:34:38 +0600

    update nuxt.config.js

--- components/navtopComponent.vue
+++ components/navtopComponent.vue
@@ -101,32 +101,6 @@
               @click="toggle"
               class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
             >
-              <!-- <svg
-                :class="[isOpen ? 'hidden' : 'block', 'h-6 w-6']"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg> -->
-              <!-- <svg
-                :class="[isOpen ? 'block' : 'hidden', 'h-6 w-6']"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg> -->
               <!-- Button NEW  open menu-->
               <svg
                 :class="[isOpen ? 'hidden' : 'block', 'h-6 w-6']"
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,16 @@ export default {
       rel: 'icon',
       type: 'image/x-icon',
       href: '/favicon.ico'
-    }],
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com'
+    },
+    {
+      href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap',
+      rel: 'stylesheet'
+    }
+  ],
 
   },
 
