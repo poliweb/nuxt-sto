fa183f622c9864b04c2eee4d1d6747a34303952b
web@poliweb.su <web@poliweb.su>
2021-06-07 14:52:37 +0600

    Update Logo.vue

--- components/Logo.vue
+++ components/Logo.vue
@@ -4,3 +4,16 @@
     <img src="~/assets/img/mobil_logo.png" alt="logo" class="sm:block md:hidden">
   </div>
 </template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
